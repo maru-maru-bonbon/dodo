ae19a3bbc114443f4d5dbb45714e7c8424c9d3ec
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2021-02-04 08:59:00 +0900

    userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many :plans
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: "は半角英数字で入力して下さい"
+         
+         validates :email, uniqueness: { case_sensitive: false }         
+         validates :nickname, presence: true
+      
 end
